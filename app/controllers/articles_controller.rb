5567cfbda703a3d7d4e53e8b1d6655478c365099
Tanya <tanyairina@hotmail.com>
2021-05-31 20:10:20 -0600

    adding validations and category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
 
-before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    
     def show
     end
 
     def index
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 5)
     end
 
     def new
@@ -18,7 +21,7 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:notice] = "Article was created succesfully"
             redirect_to @article
@@ -52,4 +55,10 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
         params.require(:article).permit(:title, :description)
     end
 
+    def require_same_user
+        if current_user != @article.user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article 
+        end
+    end
 end
